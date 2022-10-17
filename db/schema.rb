6940d188d68097abd3f0f5cf0c155421000fa969
clpiano <111512024+clpiano@users.noreply.github.com>
2022-10-17 02:43:30 +0000

    Add ItemModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_15_020305) do
+ActiveRecord::Schema.define(version: 2022_10_17_022357) do
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
