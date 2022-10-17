21c1fa147db86180892e7a290c07c1c351a47c4e
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-10-17 05:32:10 +0000

    save

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2022_10_17_022540) do
 
+  create_table "destinations", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "zipcode"
+    t.string "addresss"
+    t.string "delivery_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "price_at_purchase"
+    t.integer "piece"
+    t.integer "production_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
