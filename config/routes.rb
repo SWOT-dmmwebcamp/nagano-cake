8c31db30c40b86dbb1205e7caff0c3d5b0b68ea9
clpiano <111512024+clpiano@users.noreply.github.com>
2022-10-18 16:28:07 +0000

    Add ItemController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :users
+
+  namespace :admin do
+    resources :items
+    
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
