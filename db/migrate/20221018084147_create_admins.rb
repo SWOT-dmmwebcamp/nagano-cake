6c8fb42616eaa5b1fce58d89283def5d9417d875
EmiSuzuki <mush.2525miumiu@gmail.com>
2022-10-19 00:27:18 +0000

    コントローラ・ビュー追加

--- db/migrate/20221018084147_create_admins.rb
+++ db/migrate/20221018084147_create_admins.rb
@@ -1,11 +1,8 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-      t.string  :email,     null: false
-      t.string  :password,  null: false
+
       t.timestamps
     end
-    
-    add_index :admins, :email,  unique: true
   end
 end
