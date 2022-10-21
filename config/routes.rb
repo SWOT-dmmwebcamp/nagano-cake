4536fb8dee8aab20551192f48766ffc99cc8e51c
clpiano <111512024+clpiano@users.noreply.github.com>
2022-10-21 05:07:17 +0000

    ?

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
 
+
   devise_for :customers
   devise_for :users
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  scope module: :public do
-    root to: 'homes#top'
-    get "/about" => "homes#about" , as: "about"
-    resources :items, only: [:index, :show]
-    resources :registrations, only: [:new, :create]
-    resources :sessions, only: [:new, :create, :destroy]
-    resources :customers
-    resources :cart_items
-    resources :orders
-    resources :deliverys, only: [:index, :edit, :update, :create, :destroy]
-  end
+
+  get '/cart_items' => 'public/cart_items#index'
 
   namespace :admin do
     root to: 'homes#top'
@@ -27,5 +19,4 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :edit, :update, :create]
     resources :customers, only: [:index,:show,:edit,:update]
   end
-  
 end
