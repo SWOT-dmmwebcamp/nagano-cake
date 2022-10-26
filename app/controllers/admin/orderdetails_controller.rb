6622511580eb7a91a34d30800038efdf028f56ce
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-10-26 08:13:25 +0000

    save

--- app/controllers/admin/orderdetails_controller.rb
+++ app/controllers/admin/orderdetails_controller.rb
@@ -3,7 +3,28 @@ class Admin::OrderdetailsController < ApplicationController
   def update
     @order_detail = Orderdetail.find(params[:id])
     @order_detail.update(detail_params)
-    redirect_to admin_order_path(@order_detail.order_id)
+    
+    if params[:item_status] == "2" then
+      order = Order.find(@order_detail.order_id)
+      order.update(order_status: "making")
+    end
+    
+    @test = 0
+    @n = 0
+    item_status = Orderdetail.where(order_id: @order_detail.order_id).pluck(:item_status)
+    item_status.each do |s|
+      unless s == 3 then
+        @test = 1
+      end
+      @n += 1
+    end
+    
+    if @test == 0
+      order = Order.find(@order_detail.order_id)
+      order.update(order_status: "send_prepare")
+    end
+    
+    redirect_to request.referer
   end
   
   
