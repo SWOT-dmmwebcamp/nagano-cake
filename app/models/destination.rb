3780d1bdffe22223b2cdb753e7196dfc2fee6d22
EmiSuzuki <mush.2525miumiu@gmail.com>
2022-10-27 00:43:11 +0000

    バリデーション機能追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,7 +2,7 @@ class Destination < ApplicationRecord
   
     belongs_to :customer
     
-  validates :zipcode, presence: true
+  validates :zipcode, presence: true, numericality: { only_integer: true}, length:{ minimum: 7, maximum: 7}
   validates :address, presence: true
   validates :delivery_name, presence: true
   
